0428a262357d976b4d439d7707017ed0f8824fbe
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-31 14:02:44 -0400

    Adds game constuctor

---
+++ game/game.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function Game() {
+
+}
+
+module.exports = Game;
---
+++ tests/test_game.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const mocha = require('mocha');
+const should = require('should');
+const Game = require('../game/game');
