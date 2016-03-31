a9838b0e0307ac3b831c243986b83e4172b35bcf
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-31 17:51:43 -0400

    Deletes all tests and modifies player constructor
    * The unit tests were silly for such low-level functions
    * All methods now return something
      * Procedures with side effects return true or false depending on
      whether they actually did the side-effects or not

--- game/player.js
+++ game/player.js
@@ -11,26 +11,49 @@ function Player(alias) {
     this.numNukes++;
     this.countermeasureEffectiveness = 1;
     this.hasUsedCountermeasures = false;
+
+    return true;
   };
 
   this.launch = function() {
-    this.numNukes = 0;
-    this.countermeasureEffectiveness = 1;
-    this.hasUsedCountermeasures = false;
+    var result;
+    if(this.numNukes > 0){
+      this.numNukes = 0;
+      this.countermeasureEffectiveness = 1;
+      this.hasUsedCountermeasures = false;
+      result = true;
+    } else {
+      result = false;
+    }
+
+    return result;
   };
 
   this.deployCountermeasures = function() {
     this.hasUsedCountermeasures = true;
     this.countermeasureEffectiveness *= 0.5;
+    return true;
   };
 
-  this.destroyCity = function() {
-    this.numCities--;
+  this.destroyCities = function(cnt) {
+    this.numCities -= cnt;
+    return true;
   };
 
   this.hasLost = function() {
     return this.numCities <= 0;
   };
+
+  this.getState = function() {
+    return {
+      alias: this.alias,
+      numCities: this.numCities,
+      numNukes: this.numNukes,
+      countermeasureEffectiveness: this.countermeasureEffectiveness,
+      hasUsedCountermeasures: this.hasUsedCountermeasures
+    };
+  };
+
 }
 
 module.exports = Player;
