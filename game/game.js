06a0d211e7788218946375d00742be704d2e51a0
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-31 14:23:12 -0400

    Adds some functions and tests to Game constructor

--- game/game.js
+++ game/game.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function Game() {
+  this.players = [];
+  this.isOver = false;
 
+  this.addPlayer = function(newPlayer) {
+    this.players.push(newPlayer);
+  };
 }
 
 module.exports = Game;
--- tests/test_game.js
+++ tests/test_game.js
@@ -3,3 +3,33 @@
 const mocha = require('mocha');
 const should = require('should');
 const Game = require('../game/game');
+const Player = require('../game/player');
+
+describe('Game constructor', function(){
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it('should have a players property which is an array', function(){
+    game.should.have.property('players').which.is.an.Array();
+  });
+
+  it('should have a isOver property with a default value of false', function(){
+    game.should.have.property('isOver').which.is.false();
+  });
+
+  describe('addPlayer function', function(){
+    beforeEach(function() {
+      var player = new Player();
+
+      game.addPlayer(player);
+    });
+
+    it('should add a player to the players array', function(){
+      game.players.length.should.be.exactly(1);
+    });
+  });
+
+});
