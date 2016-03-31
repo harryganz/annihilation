d3aeb8e443d7cf330bbce3396ebcffc223ed0028
Harry Ganz <harry.y.ganz@gmail.com>
2016-03-31 13:39:25 -0400

    Adds deployCountermeasures function to Player

--- game/player.js
+++ game/player.js
@@ -9,11 +9,22 @@ function Player(alias) {
 
   this.stockpile = function() {
     this.numNukes++;
+    this.countermeasureEffectiveness = 1;
+    this.hasUsedCountermeasures = false;
   };
 
   this.launch = function() {
     this.numNukes = 0;
+    this.countermeasureEffectiveness = 1;
+    this.hasUsedCountermeasures = false;
   };
+
+  this.deployCountermeasures = function() {
+    this.hasUsedCountermeasures = true;
+    this.countermeasureEffectiveness *= 0.5;
+  };
+
+
 }
 
 module.exports = Player;
