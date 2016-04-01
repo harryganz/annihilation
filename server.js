4800821dfe8b64243b7bd59d9ca182e30d760828
Harry Ganz <harry.y.ganz@gmail.com>
2016-04-01 13:46:04 -0400

    Adds catch-all route on server
    * Use catch-all route to allow react to handle routing

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const io = require('socket.io')(server);
 // App Wide Middlewares
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Do routing on client-side
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 var port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('listening on port ', port);
