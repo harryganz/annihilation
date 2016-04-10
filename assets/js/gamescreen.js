7d0cbfc4effa855a9ba3317dac00e2538eb03315
Harry Ganz <harry.y.ganz@gmail.com>
2016-04-10 17:51:33 -0400

    Adds countdown when game starts

--- assets/js/gamescreen.js
+++ assets/js/gamescreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import $ from 'jquery';
 
 import Waiting from './waiting';
 import Buttons from './buttons';
 import UserPanel from './user-panel';
 
 const GameScreen = React.createClass({
+  getInitialState: function() {
+    return {
+      firstTurn: true
+    };
+  },
   contextTypes: {
     router: React.PropTypes.object,
     waiting: React.PropTypes.bool,
@@ -16,6 +22,27 @@ const GameScreen = React.createClass({
       this.context.router.replace('/');
     }
   },
+  startCount: function() {
+    if(this.state.firstTurn){
+      var count = 2;
+      var $countdown = $('.countdown');
+      $countdown.show();
+      var intId;
+      intId = setInterval(() => {
+        if(count > 0){
+          $countdown.text(count);
+          count--;
+        } else if(count === 0) {
+          $countdown.text("GO!");
+          count--;
+        } else {
+          $countdown.hide();
+          clearInterval(intId);
+        }
+      }, 1000);
+      this.setState({firstTurn: false});
+    }
+  },
   render: function() {
     return (
       <div className="game-screen">
@@ -23,7 +50,11 @@ const GameScreen = React.createClass({
           <UserPanel game={this.context.game} id={0} key={0} />
           <UserPanel game={this.context.game} id={1} key={1} />
         </div>
-        {this.context.waiting ? <Waiting /> : <Buttons alias={this.context.alias}/>}
+        <div className="countdown panel">
+            3
+        </div>
+        {this.context.waiting ? <Waiting /> : <Buttons alias={this.context.alias}
+          ref={this.startCount}/>}
       </div>
     );
   }
