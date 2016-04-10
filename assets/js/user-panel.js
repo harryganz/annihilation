e1665cea56a6c47498d14b02e3d058a078290b9d
Harry Ganz <harry.y.ganz@gmail.com>
2016-04-10 16:19:00 -0400

    Adds city graphics
    * Display a little city for each city a user has

--- assets/js/user-panel.js
+++ assets/js/user-panel.js
@@ -3,11 +3,18 @@ import React from 'react';
 const UserPanel = React.createClass({
   render: function() {
     var player = this.props.game.players[this.props.id];
+    var cities = [];
+    if(player){
+      for (var i = 0; i < player.numCities; i++) {
+        cities.push(<img src="/img/city.png" alt="city" key={i} className="graphic"/>);
+      }
+    }
+
     return (
       <div className="user-panel">
         <h3>{player ? player.alias : 'NA'}</h3>
         <div className="panel">
-          <p>Cities left: {player ? player.numCities : 'NA'}</p>
+          <p>Cities left: {player ? cities : 'NA'}</p>
           <p>Nukes Stockpiled: {player ? player.numNukes : 'NA'}</p>
           <p>Countermeasure Effectiveness: {player ? player.countermeasureEffectiveness : 'NA'}</p>
         </div>
